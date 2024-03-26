91f0755698bf78a24837aaaf1ac495f1e07ec1c3
sebnem yayla <sebnem.sara.yayla@gmail.com>
2024-03-26 18:45:23 +0100

    Update test_results.py

--- prevalence_pipeline/lib/test_results.py
+++ prevalence_pipeline/lib/test_results.py
@@ -6,12 +6,12 @@ def testing_results(loaded_data, selected_year, selected_lemma):
         for year, text in years_words.items():
             if year==selected_year:
                 total_blogs.append(blog_name)
-                words = text.split()  # Assume 'text' is a string of words separated by spaces
+                words = text.split()  
                 for word in words:
                     if word==selected_lemma:
                         blogs_containing_word.append({
                             'blog_name': blog_name,
                             'year': int(year),
-                            'word': word  # Ensure this is a single word (string), not a list
+                            'word': word  
                         })
     return list(set(val for dic in blogs_containing_word for val in dic.values())), set(total_blogs)
