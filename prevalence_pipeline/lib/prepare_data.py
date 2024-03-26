c72dff1a4bcc6c41b28f55d47c7540ea6e96cb14
sebnem yayla <sebnem.sara.yayla@gmail.com>
2024-03-26 18:44:56 +0100

    Update prepare_data.py

--- prevalence_pipeline/lib/prepare_data.py
+++ prevalence_pipeline/lib/prepare_data.py
@@ -1,15 +1,14 @@
 import pandas as pd
 
 def prepare_data(_loaded_data):
-    rows = []  # This will hold the individual words, each as a separate row
+    rows = [] 
     for blog_name, years_words in _loaded_data.items():
         for year, text in years_words.items():
-            words = text.split()  # Assume 'text' is a string of words separated by spaces
+            words = text.split()  
             for word in words:
-                # Append each word as a separate row in the DataFrame
                 rows.append({
                     'blog_name': blog_name,
                     'year': int(year),
-                    'word': word  # Ensure this is a single word (string), not a list
+                    'word': word  
                 })
     return pd.DataFrame(rows)
